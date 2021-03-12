b830ebe702eda8d56c051b30a8e11540e2f4a21b
Calista Mayer <calista.a.mayer@gmail.com>
2021-03-11 20:04:31 -0800

    submit pet form

--- client/src/pages/SubmitPet.js
+++ client/src/pages/SubmitPet.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import { Container, Form, Header, Button, Segment, Grid } from 'semantic-ui-react';
 
 const SubmitPet = () => {
     return (
         <section>
-            <h2>Submit A Pet</h2>
-            <p>Form goes here</p>
+            <Container className="topPadding">
+                <Header as="h1" textAlign="center">Submit A Pet</Header>
+                <Segment>
+                    <Form>
+                        <Form.Group widths="equal">
+                            <Form.Input label="Name">
+                                <input placeholder='Pet name' />
+                            </Form.Input>
+                            <Form.Field label="Type" control='select'>
+                                <option value='cat'>Cat</option>
+                                <option value='dog'>Dog</option>
+                            </Form.Field>
+
+                            <Form.Field label='Sex' control='select'>
+                                <option value='male'>Male</option>
+                                <option value='female'>Female</option>
+                            </Form.Field>
+                            <Form.Field label='Age' control='select'>
+                                <option value='young'>Young</option>
+                                <option value='adult'>Adult</option>
+                                <option value='senior'>Senior</option>
+                            </Form.Field>
+                        </Form.Group>
+                        <Form.Group widths='2'>
+                            <Form.Group grouped>
+                                <label>Behavior</label>
+                                <Form.Field label='No kids' control='input' type='checkbox' />
+                                <Form.Field label='Only pet' control='input' type='checkbox' />
+                                <Form.Field label='No cats' control='input' type='checkbox' />
+                                <Form.Field label='No dogs' control='input' type='checkbox' />
+                            </Form.Group>
+                            <Form.Group grouped>
+                                <label>Medical Condition?</label>
+                                <Form.Field label='yes' control='input' type='checkbox' />
+                            </Form.Group>
+                        </Form.Group>
+
+                        <Grid>
+                            <Grid.Column textAlign="center">
+                                <Button type='submit' fluid color='blue'>Submit</Button>
+                            </Grid.Column>
+                        </Grid>
+                    </Form>
+                </Segment>
+            </Container>
         </section>
     );
 };
