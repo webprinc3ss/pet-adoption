83bf3f1cf01c420c44ffac97c6e0d4e22656f473
Anthony Kieu <anthonykieu@gmail.com>
2021-03-13 01:23:14 -0500

    added cloudinary server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const { cloudinary } = require('./utils/cloudinary');
 const express = require('express');
+
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
@@ -10,6 +12,7 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+var cors = require('cors');
 
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
@@ -20,9 +23,10 @@ const server = new ApolloServer({
 
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.static('public'));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(cors());
 
 // if we're in production (heroku), serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -31,6 +35,30 @@ if (process.env.NODE_ENV === 'production') {
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.get('/api/images', async (req, res) => {
+  const { resources } = await cloudinary.search
+      .expression('folder:Pets')
+      .sort_by('public_id', 'desc')
+      .max_results(30)
+      .execute();
+
+  const publicIds = resources.map((file) => file.public_id);
+  res.send(publicIds);
+});
+app.post('/api/upload', async (req, res) => {
+  try {
+      const fileStr = req.body.data;
+      const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+          upload_preset: 'Pets',
+      });
+      console.log(uploadResponse);
+      res.json({ msg: 'uploaded' });
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ err: 'Something went wrong' });
+  }
 });
 
 db.once('open', () => {
