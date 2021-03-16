3d7dc32def98d6585f249133492cbffdf3039d24
Calista Mayer <calista.a.mayer@gmail.com>
2021-03-15 17:54:59 -0700

    mission page

--- client/src/pages/Mission.js
+++ client/src/pages/Mission.js
@@ -9,8 +9,19 @@ const Mission = () => {
                 <Grid.Column>
                     <section>
 
-                        <p>Hi!  I am Eevee and I am an ambassador for MustFit Pets. This is my story and why I want to help other hard-to-place-pets find a furever home. </p>
-                    </section></Grid.Column>
+                        <p>
+                            Hi! I am Eevee, a sweet and gentle chihuahua! Until I was 11 years old, I was a "breeder" for my owner. 
+                            I had thousands of puppies througout my life! But, my owner did not care about my health :( When I was found 
+                            by a pet rescue organization, I had no teeth and my leg was so injured, it had to be removed. I learned to 
+                            get around on three legs, though, and I still had a heart of gold. When my new mom Ellie found me, I was really nervous
+                            at first... when I saw how gentle and caring she was, I felt comfortable showing her my silly side! We took 
+                            so many naps and adventures together; I am so lucky that I got a second chance at the magical life I deserved!
+                            Now, I am an ambassador for MustFit Pets. This is my story and why I want to help other hard-to-place-pets find a furever home. </p>
+                    </section>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <img src="./assets/images/eevee.png" alt="Eeevee the chihuahua" />
+                    </Grid.Column>
             </Grid>
         </Container>
     );
