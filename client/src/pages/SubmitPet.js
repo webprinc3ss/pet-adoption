937f002424537ae54e0df46225a1cf557cf571c2
Mystic Kitty <djabranton@gmail.com>
2021-03-16 11:54:42 -0700

    Added new fields to petsubmit.js.

--- client/src/pages/SubmitPet.js
+++ client/src/pages/SubmitPet.js
@@ -10,9 +10,9 @@ const SubmitPet = () => {
     const [selectedFile, setSelectedFile] = useState();
     const [previewSource, setPreviewSource] = useState('');
     const [showAlert, setShowAlert] = useState(false);
-    // const [formState, setFormState] = useState({ name: '', type: '', sex: '', ageClass: '', kids: '', onlyPet: '', otherCats: '', otherDogs: '', medical: '', about: '' });
+    // const [formState, setFormState] = useState({ name: '', type: '', sex: '', ageClass: '', kids: '', otherCats: '', otherDogs: '', medical: '', about: '' });
     const [createPet, { error }] = useMutation(CREATE_PET);
-    
+
     //takes photo file in and saves to state
     const handleFileInputChange = (e) => {
         const file = e.target.files[0];
@@ -58,88 +58,103 @@ const SubmitPet = () => {
                             <Form.Input
                                 label="Name"
                                 name="name"
-                                // value={formState.name}
-                                >
+                            // value={formState.name}
+                            >
                                 <input placeholder='Pet name'
                                 />
                             </Form.Input>
-                            <Form.Field label="Type" 
-                            control='select' 
-                            name="type"
+                            <Form.Field label="Type"
+                                control='select'
+                                name="type"
                             // value={formState.type}
                             >
                                 <option value='cat'>Cat</option>
                                 <option value='dog'>Dog</option>
                             </Form.Field>
 
-                            <Form.Field 
-                            label='Sex' 
-                            control='select' 
-                            name="sex"
+                            <Form.Field
+                                label='Sex'
+                                control='select'
+                                name="sex"
                             // value={formState.sex}
                             >
                                 <option value='M'>Male</option>
                                 <option value='F'>Female</option>
                             </Form.Field>
-                            <Form.Field 
-                            label='Age' 
-                            control='select' 
-                            name="ageClass"
+                            <Form.Field
+                                label='Age Category'
+                                control='select'
+                                name="ageClass"
                             // value={formState.ageClass}
                             >
                                 <option value='young'>Young</option>
                                 <option value='adult'>Adult</option>
                                 <option value='senior'>Senior</option>
                             </Form.Field>
+                            <Form.Field
+                                label='Size'
+                                control='select'
+                                name="size"
+                            // value={formState.ageClass}
+                            >
+                                <option value='extra-small'>Extra Small</option>
+                                <option value='small'>Small</option>
+                                <option value='medium'>medium</option>
+                                <option value='large'>large</option>
+                                <option value='extra-large'>extra-large</option>
+                            </Form.Field>
                         </Form.Group>
-                        <Form.Group widths='2'>
-                            <Form.Group grouped 
-                            name="behavior">
-                                <label>Behavior</label>
-                                <Form.Field label='kids' name="kids" control='input' type='checkbox' // value={formState.kids}
-                                    />
-                                <Form.Field label='only pet' name="onlyPet" control='input' type='checkbox' // value={formState.onlyPet}
-                                    />
-                                <Form.Field label='other cats' name="otherCats" control='input' type='checkbox' // value={formState.otherCats}
-                                    />
-                                <Form.Field label='other dogs' name="otherDogs" control='input' type='checkbox' // value={formState.otherDogs}
-                                    />
-                            </Form.Group>
-                            <Form.Group grouped>
-                                <Input label='Photo Upload'
-                                    id="fileInput"
-                                    type="file"
-                                    name="photo" //photo???
-                                    onChange={handleFileInputChange}
-                                    value={fileInputState}
-                                    className="form-input"
-                                    // style={{ width: "370px" }}
-                                />
-
-                                {previewSource && (
-                                    <img
-                                        src={previewSource}
-                                        alt="chosen"
-                                        style={{ height: '300px' }}
-                                    />
-                                )}
-
-                            </Form.Group>
-                        </Form.Group>
-                        <Form.Group widths="2">
-                            <Form.Group grouped>
+                        <Form.Group>
+                            <Form.Group grouped widths="equal">
                                 <label>Medical Condition?</label>
                                 <Form.Field label='yes' control='input' type='checkbox' name="medical" />
-                            </Form.Group>
-                            <Form.Group grouped>
 
-                                <Form.Field label='Tell us about this pet' control='input' type='textarea' name="about"
-                                    style={{ width: "400px", height: "200px" }}
-
-
+                                <label>Behavior</label>
+                                <Form.Field label='Can live with kids' name="kids" control='input' type='checkbox' // value={formState.kids}
+                                />
+                                <Form.Field label='Can live with cats' name="Cats" control='input' type='checkbox' // value={formState.otherCats}
+                                />
+                                <Form.Field label='Can live with dogs' name="Dogs" control='input' type='checkbox' // value={formState.otherDogs}
                                 />
                             </Form.Group>
+                            <Form.Group>
+                                <Form.Field label='Tell us about this pet' control='input' type='textarea' name="about"
+                                    style={{ width: "400px", height: "150px" }}
+                                />
+                            </Form.Group>
+                            <Form.Group grouped>
+                                <Form.Input
+                                    label="Age"
+                                    name="age"
+                                    style={{ width: "100%" }}
+                                // value={formState.name}
+                                >
+                                    <input placeholder='Pet age'
+                                    />
+                                </Form.Input>
+                                <Form.Group grouped >
+                                    <Input label='Photo Upload'
+                                        id="fileInput"
+                                        type="file"
+                                        name="photo" //photo???
+                                        onChange={handleFileInputChange}
+                                        value={fileInputState}
+                                        className="form-input"
+                                    // style={{ width: "370px" }}
+                                    />
+
+                                    {previewSource && (
+                                        <img
+                                            src={previewSource}
+                                            alt="chosen"
+                                            style={{ height: '300px' }}
+                                        />
+                                    )}
+                                </Form.Group>
+                            </Form.Group>
+
                         </Form.Group>
+
                         <Grid>
                             <Grid.Column textAlign="center">
                                 <Button type='submit' fluid color='blue'>Submit</Button>
@@ -148,7 +163,7 @@ const SubmitPet = () => {
                     </Form>
                 </Segment>
             </Container>
-        </section>
+        </section >
     );
 };
 
