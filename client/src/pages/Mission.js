4da9ef4e34da68d6deccf1d61e160f07e101807b
Calista Mayer <calista.a.mayer@gmail.com>
2021-03-15 18:31:19 -0700

    mission

--- client/src/pages/Mission.js
+++ client/src/pages/Mission.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Header, Grid } from 'semantic-ui-react';
+import { Container, Form, Header, Button, Segment, Grid, Image } from 'semantic-ui-react';
+import eevee from "../assets/images/eevee.png";
 
 const Mission = () => {
     return (
@@ -7,21 +8,27 @@ const Mission = () => {
             <Header as='h1' textAlign="center">Eevee's Mission</Header>
             <Grid centered columns={2}>
                 <Grid.Column>
+                    <Image src={eevee} />
+                </Grid.Column>
+                <Grid.Column textAlign="justified">
                     <section>
 
                         <p>
-                            Hi! I am Eevee, a sweet and gentle chihuahua! Until I was 11 years old, I was a "breeder" for my owner. 
-                            I had thousands of puppies througout my life! But, my owner did not care about my health :( When I was found 
-                            by a pet rescue organization, I had no teeth and my leg was so injured, it had to be removed. I learned to 
-                            get around on three legs, though, and I still had a heart of gold. When my new mom Ellie found me, I was really nervous
-                            at first... when I saw how gentle and caring she was, I felt comfortable showing her my silly side! We took 
-                            so many naps and adventures together; I am so lucky that I got a second chance at the magical life I deserved!
-                            Now, I am an ambassador for MustFit Pets. This is my story and why I want to help other hard-to-place-pets find a furever home. </p>
+                            Hi! I am Eevee, a sweet and gentle chihuahua! Until I was 11 years old, I was owned by a breeder
+                            who did not care enough about my health. When I was found by a pet rescue organization, I had no 
+                            teeth and my leg was so injured, it had to be removed. I learned to get around on three legs, though, 
+                            and I still had a heart of gold. <br /><br />
+                            
+                            When my new mom Ellie adopted me, I was really nervous at first... when I saw how caring she was, I felt 
+                            comfortable showing her my silly side! I am so lucky that I got a second chance at the magical life I deserved!
+                            <br /><br />
+
+                            Now, I am an ambassador for MustFit Pets. This is my story and why I want to help other hard-to-place-pets 
+                            find a furever home. 
+                        </p>
                     </section>
                     </Grid.Column>
-                    <Grid.Column>
-                        <img src="./assets/images/eevee.png" alt="Eeevee the chihuahua" />
-                    </Grid.Column>
+
             </Grid>
         </Container>
     );
