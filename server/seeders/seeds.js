d5378de04ea8b39b1bc510d8073f1cdef6da0e72
Mystic Kitty <djabranton@gmail.com>
2021-03-17 14:28:45 -0700

    Fixed seed to medical Y or N.

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -4,74 +4,74 @@ const db = require('../config/connection');
 const { User, Pet } = require('../models');
 
 db.once('open', async () => {
-    await User.deleteMany({});
-    await Pet.deleteMany({});
-  
-    // create user data
-    const userData = [];
-  
-    for (let i = 0; i < 10; i += 1) {
-      const username = faker.internet.userName();
-      const email = faker.internet.email(username);
-     // const password = faker.internet.password();
-      const password = "hunter2";
-  
-      // push User attributes to userData array
-      userData.push({ username, email, password });
+  await User.deleteMany({});
+  await Pet.deleteMany({});
+
+  // create user data
+  const userData = [];
+
+  for (let i = 0; i < 10; i += 1) {
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    // const password = faker.internet.password();
+    const password = "hunter2";
+
+    // push User attributes to userData array
+    userData.push({ username, email, password });
+  }
+
+  const createdUsers = await User.collection.insertMany(userData);
+
+  // create Pets
+  let petData = [];
+
+  for (let i = 0; i < 20; i += 1) {
+
+    // create random name
+    const name = faker.name.firstName();
+
+    // create pet type
+    const type = faker.random.arrayElement(["dog", "cat"]);
+    // create random age
+    const age = faker.random.number(15);
+
+    // create ageClass
+    const ageClass = faker.random.arrayElement(["young", "adult", "senior"]);
+
+    // create animal size
+    const size = faker.random.arrayElement(["small", "medium", "large"]);
+
+    // create kids compatibility
+    const kids = faker.random.arrayElement(["Y", "N"]);
+
+    // create cats compatibility
+    const cats = faker.random.arrayElement(["Y", "N"]);
+
+    // create dogs compatibility
+    const dogs = faker.random.arrayElement(["Y", "N"]);
+
+    // create medical
+    const medical = faker.random.arrayElement(["Y", "N"]);
+
+    // create random animal image
+    if (type === "cat") {
+      photo = faker.image.cats();
+    } else {
+      photo = faker.image.animals();
     }
-  
-    const createdUsers = await User.collection.insertMany(userData);
-  
-    // create Pets
-    let petData = [];
-    
-    for (let i = 0; i < 20; i += 1) {
-      
-        // create random name
-        const name = faker.name.firstName();
 
-        // create pet type
-        const type = faker.random.arrayElement(["dog", "cat"]);
-        // create random age
-        const age = faker.random.number(15);
+    // create animal sex
+    const sex = faker.random.arrayElement(["M", "F"]);
 
-        // create ageClass
-        const ageClass = faker.random.arrayElement(["young", "adult", "senior"]);
+    // create about text
+    const about = faker.lorem.words(Math.round(Math.random() * 20) + 1);
 
-        // create animal size
-        const size = faker.random.arrayElement(["small", "medium", "large"]);
+    petData.push({ name, type, age, ageClass, size, kids, cats, dogs, medical, photo, sex, about });
 
-        // create kids compatibility
-        const kids = faker.random.arrayElement(["Y", "N"]);
+  }
 
-        // create cats compatibility
-        const cats = faker.random.arrayElement(["Y", "N"]);
+  const createdPets = await Pet.collection.insertMany(petData);
 
-        // create dogs compatibility
-        const dogs = faker.random.arrayElement(["Y", "N"]);
-        
-        // create medical
-        const medical = faker.random.arrayElement([true, false]);
-
-        // create random animal image
-        if (type === "cat"){
-          photo = faker.image.cats();
-        } else {
-          photo = faker.image.animals();
-        }
-        
-        // create animal sex
-        const sex = faker.random.arrayElement(["M", "F"]);
-
-        // create about text
-        const about = faker.lorem.words(Math.round(Math.random() * 20) + 1);
-
-        petData.push({ name, type, age, ageClass, size, kids, cats, dogs, medical, photo, sex, about });
-
-    }
-  
-    const createdPets = await Pet.collection.insertMany(petData);
-    
-    console.log('Seeding is done!');
-    process.exit(0);
-  });
+  console.log('Seeding is done!');
+  process.exit(0);
+});
